97096fc35a3fae89f63ff54ad02a63f41289138d
Maxim <mresnyansky@gmail.com>
2023-06-30 18:28:11 +0200

    1 map and do next

--- Assets/Scenes/LoadSceneTrigger.cs
+++ Assets/Scenes/LoadSceneTrigger.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneTrigger : MonoBehaviour
 {
+    public int sceneIndex = 0; // Индекс сцены для загрузки
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(sceneIndex);
         }
     }
 }
